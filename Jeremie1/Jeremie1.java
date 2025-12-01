95a754fce0bb537cd8771f630b641de19a54cc58
Jeremie252007 <jeremiecotillas03252007@gmail.com>
2025-12-01 09:19:27 +0800

    Add files via upload

---
+++ Jeremie1/Jeremie1.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+
+public class Jeremie1
+{
+   public static void main(String[]args){
+       SwingUtilities.invoker(() -> {
+           JFrame frame = new  JFrame("Programming Problems Solver");
+           framw.setDefaultCloser
+       }
+       
+   }
+}
